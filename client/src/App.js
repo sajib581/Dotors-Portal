b12b4572a84d63f2b3db4a3f44e67293f45cacf1
sajib121 <sajibdas581@gmail.com>
2020-10-10 13:33:08 +0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css';
+import Home from './Components/Home/Home/Home';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
